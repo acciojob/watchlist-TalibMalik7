643ecef0e018d7079487e2403ebf8b4ef8c8268f
TalibMalik7 <mohammedtalib5@gmail.com>
2023-02-25 01:58:44 +0530

    first

---
+++ src/main/java/com/driver/Movie.java
@@ -0,0 +1,39 @@
+package com.driver;
+
+//Movie.java - String name, int durationInMinutes, double imdbRating, no-args constructor,
+// all-args constructor and getters-setters
+public class Movie {
+    private String name;
+    private int duratonInMinutes;
+    private double imdbRating;
+    Movie(){}
+    Movie(String name , int dur, double rating){
+        this.name = name;
+        this.duratonInMinutes = dur;
+        imdbRating = rating;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getDuratonInMinutes() {
+        return duratonInMinutes;
+    }
+
+    public void setDuratonInMinutes(int duratonInMinutes) {
+        this.duratonInMinutes = duratonInMinutes;
+    }
+
+    public double getImdbRating() {
+        return imdbRating;
+    }
+
+    public void setImdbRating(double imdbRating) {
+        this.imdbRating = imdbRating;
+    }
+}
